ae1c78f407d8d0718c79c789bb44f6b81c42e925
Idan Moshe <idanmos@gmail.com>
2020-08-02 01:02:34 +0300

    Initial Commit

---
+++ Animal Food/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animal Food
+//
+//  Created by Idan Moshe on 02/08/2020.
+//  Copyright © 2020 Idan Moshe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
